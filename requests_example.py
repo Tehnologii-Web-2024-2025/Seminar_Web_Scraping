3609a6e1a8ad99320ecd137e8ae83b99ed50339a
MichaelXcX <ciuciuletemihaicm@gmail.com>
2024-10-23 17:40:59 +0300

    Gata seminarul

--- requests_example.py
+++ requests_example.py
@@ -1,14 +1,43 @@
 import requests
+from bs4 import BeautifulSoup
+import lxml
+# url = 'https://www.youtube.com'
 
-url = 'https://www.youtube.com'
+# req = requests.get(url)
+
+# print(req.status_code)
+# print("")
+# print(req.text)
+# print("")
+# print(req.headers)
+
+# file = open("youtube.html", "w")
+# file.write(req.text)
+
+url = "https://digi24.ro/robots.txt"
 
 req = requests.get(url)
 
-print(req.status_code)
-print("")
-print(req.text)
-print("")
-print(req.headers)
+# print(req.text)
 
-file = open("youtube.html", "w")
-file.write(req.text)
+sitemap = "https://digi24.ro/sitemaps/sitemap-index.xml"
+req = requests.get(sitemap)
+
+soup = BeautifulSoup(req.text, 'xml')
+# print(soup.prettify())
+
+locs = soup.find_all("loc")
+
+for loc in locs:
+    loc = loc.text
+    articles = requests.get(loc)
+    articles_soup = BeautifulSoup(articles.text, 'xml')
+
+    articles_urls = articles_soup.find_all("loc")
+    for article_url in articles_urls:
+        article = requests.get(article_url.text)
+        article_soup = BeautifulSoup(article.text, "html.parser")
+        title = article_soup.find("h1").text
+
+        print(title)
+
